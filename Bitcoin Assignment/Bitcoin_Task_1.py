5c82e197cfa7f2a24f31334a030f98642090e793
Stratos Grigoroudis <61066632+StratosGrig@users.noreply.github.com>
2021-05-01 12:29:22 +0300

    Add files via upload

--- Bitcoin Assignment/Bitcoin_Task_1.py
+++ Bitcoin Assignment/Bitcoin_Task_1.py
@@ -4,36 +4,34 @@ from bitcoinutils.keys import P2pkhAddress, P2shAddress, PrivateKey
 from bitcoinutils.script import Script
 from bitcoinutils.constants import TYPE_RELATIVE_TIMELOCK
 import argparse
-import time
 
 
 def main():
     # always remember to setup the network
-    setup('testnet')
+    setup('regtest')
 
     #
     # This script creates a P2SH address containing a CHECKSEQUENCEVERIFY plus
-    # a P2PKH locking funds with a key as well as for 20 blocks
+    # a P2PKH locking funds with a key as well as for a relative amount of blocks or a relative amount of seconds since the transaction.
     #
-
 
     parser = argparse.ArgumentParser(description='Give the public key, a future time expressed either in block height or in UNIX Epoch time and the P2SH address will be displayed')
     parser.add_argument('pubkey', help="Add the public key.")
-    parser.add_argument('-blocks', type= int, help = "Add the number of blocks.")
-    parser.add_argument('-time', help = " Add time")
+    parser.add_argument('-param', type= int, help = "Add the number of blocks or the time expressed in seconds.")
+    parser.add_argument('-whattype',default = True, type=lambda x: (str(x).lower() == 'true'), help = " True for Block, False for Time")
     args = parser.parse_args()
     
-    seconds = time.time()
-    local_time = time.ctime(seconds)
-    print('Seconds since epoch = ', local_time)
     
     public_key = args.pubkey 
-    relative_blocks = args.blocks
+    relative_blocks = args.param
+    is_type_block = args.whattype
         
-    print(public_key)
-    print(relative_blocks)
+    #print(public_key)
+    #print(relative_blocks)
+    #print(is_type_block)
     
-    seq = Sequence(TYPE_RELATIVE_TIMELOCK, relative_blocks)
+    seq = Sequence(TYPE_RELATIVE_TIMELOCK, relative_blocks, is_type_block )
+
 
     # secret key corresponding to the pubkey needed for the P2SH (P2PKH) transaction
     p2pkh_sk = PrivateKey(public_key)
@@ -46,7 +44,7 @@ def main():
 
     # create a P2SH address from a redeem script
     addr = P2shAddress.from_script(redeem_script)
-    print(addr.to_string())
+    print("The P2SH address is : " + addr.to_string())
 
 if __name__ == "__main__":
     main()
