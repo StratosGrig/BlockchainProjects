9492ddc3947a0150052eb2a7f43d8ad89e7a36fc
Stratos Grigoroudis <61066632+StratosGrig@users.noreply.github.com>
2021-05-02 13:51:28 +0300

    Add files via upload

--- Bitcoin Assignment/Bitcoin_Task_1.py
+++ Bitcoin Assignment/Bitcoin_Task_1.py
@@ -1,8 +1,8 @@
 from bitcoinutils.setup import setup
-from bitcoinutils.transactions import Transaction, TxInput, TxOutput, Sequence
+from bitcoinutils.transactions import Transaction, TxInput, TxOutput, Locktime
 from bitcoinutils.keys import P2pkhAddress, P2shAddress, PrivateKey
 from bitcoinutils.script import Script
-from bitcoinutils.constants import TYPE_RELATIVE_TIMELOCK
+from bitcoinutils.constants import TYPE_ABSOLUTE_TIMELOCK
 import argparse
 
 
@@ -11,27 +11,23 @@ def main():
     setup('regtest')
 
     #
-    # This script creates a P2SH address containing a CHECKSEQUENCEVERIFY plus
-    # a P2PKH locking funds with a key as well as for a relative amount of blocks or a relative amount of seconds since the transaction.
+    # This script creates a P2SH address containing a CHECKLOCKTIMEVERIFY plus
+    # a P2PKH locking funds with a key as well as for an absolute amount of blocks or an absolute amount of seconds since the transaction.
     #
 
     parser = argparse.ArgumentParser(description='Give the public key, a future time expressed either in block height or in UNIX Epoch time and the P2SH address will be displayed')
     parser.add_argument('pubkey', help="Add the public key.")
     parser.add_argument('-param', type= int, help = "Add the number of blocks or the time expressed in seconds.")
-    parser.add_argument('-whattype',default = True, type=lambda x: (str(x).lower() == 'true'), help = " True for Block, False for Time")
     args = parser.parse_args()
     
     
     public_key = args.pubkey 
-    relative_blocks = args.param
-    is_type_block = args.whattype
+    absolute_param = args.param
         
     #print(public_key)
-    #print(relative_blocks)
-    #print(is_type_block)
+    #print(absolute_param)
     
-    seq = Sequence(TYPE_RELATIVE_TIMELOCK, relative_blocks, is_type_block )
-
+    locktime = Locktime(absolute_param)
 
     # secret key corresponding to the pubkey needed for the P2SH (P2PKH) transaction
     p2pkh_sk = PrivateKey(public_key)
@@ -40,7 +36,7 @@ def main():
     p2pkh_addr = p2pkh_sk.get_public_key().get_address()
 
     # create the redeem script
-    redeem_script = Script([seq.for_script(), 'OP_CHECKSEQUENCEVERIFY', 'OP_DROP', 'OP_DUP', 'OP_HASH160', p2pkh_addr.to_hash160(), 'OP_EQUALVERIFY', 'OP_CHECKSIG'])
+    redeem_script = Script([absolute_param, 'OP_CHECKLOCKTIMEVERIFY', 'OP_DROP', 'OP_DUP', 'OP_HASH160', p2pkh_addr.to_hash160(), 'OP_EQUALVERIFY', 'OP_CHECKSIG'])
 
     # create a P2SH address from a redeem script
     addr = P2shAddress.from_script(redeem_script)
---
+++ Bitcoin Assignment/readme.txt
@@ -0,0 +1,12 @@
+Task 1
+
+For the first task you may input the following arguments :
+
+1) pubkey 
+2) -param ( a future time expressed either in block height or in UNIX Epoch time)
+
+For example you can run the following in cmd : python Bitcoin_Task1.py cRvyLwCPLU88jsyj94L7iJjQX5C2f8koG4G2gevN4BeSGcEvfKe9 -param 2000
+
+Task 2
+
+For the second task you should input the following arguments :
